85a5744a65a0bd24d8987f64fc2c0ab8b349d687
Jorge Ernesto Garcia Benavides <Jorgeedilmer@gmail.com>
2023-07-25 09:32:19 -0500

    Trabajo Nodejs

---
+++ src/controllers/repairs.controller.js
@@ -0,0 +1,134 @@
+const Repair = require('../models/repair.model');
+
+exports.findAllRepair = async (req, res) => {
+  try {
+    const repairs = await Repair.findAll({
+      where: {
+        status: 'pending',
+      },
+    });
+
+    return res.status(200).json({
+      status: 'success',
+      repairs,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: 'fail',
+      message: 'Something went very wrong! 🧨',
+    });
+  }
+};
+exports.create = async (req, res) => {
+  try {
+    const { date, userId } = req.body;
+    const repair = await Repair.create({ date, userId });
+
+    return res.status(200).json({
+      status: 'success',
+      repair,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: 'fail',
+      message: 'Something went very wrong! 🧨',
+    });
+  }
+};
+exports.findRepair = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const repair = await Repair.findOne({
+      where: {
+        id,
+        status: 'pending',
+      },
+    });
+
+    if (!repair) {
+      return res.status(404).json({
+        status: 'error',
+        message: `Repair with id: ${id} not found`,
+      });
+    }
+
+    return res.status(200).json({
+      status: 'success',
+      repair,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: 'fail',
+      message: 'Something went very wrong! 🧨',
+    });
+  }
+};
+exports.update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    // const {status} = req.body
+
+    const repair = await Repair.findOne({
+      where: {
+        id,
+        status: 'pending',
+      },
+    });
+
+    if (!repair) {
+      return res.status(404).json({
+        status: 'error',
+        message: `repair with id: ${id} not found`,
+      });
+    }
+
+    await repair.update({ status: 'completed' });
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'repair updated',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: 'fail',
+      message: 'Something went very wrong! 🧨',
+    });
+  }
+};
+exports.delete = async (req, res) => {
+  try {
+    const { id } = req.params;
+    // const {status} = req.body
+
+    const repair = await Repair.findOne({
+      where: {
+        id,
+        status: 'pending',
+      },
+    });
+
+    if (!repair) {
+      return res.status(404).json({
+        status: 'error',
+        message: `repair with id: ${id} not found`,
+      });
+    }
+
+    await repair.update({ status: 'cancelled' });
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'repair deleted',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: 'fail',
+      message: 'Something went very wrong! 🧨',
+    });
+  }
+};
---
+++ src/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const app = require('./app');
+const { db } = require('./database/config');
+
+db.authenticate()
+  .then(() => console.log('Database authenticated...🐱‍🏍🐱‍🚀'))
+  .catch((err) => console.log(err));
+
+db.sync()
+  .then(() => console.log('Database synced...🐱‍🏍🐱‍🚀'))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}...👻👿`);
+});
